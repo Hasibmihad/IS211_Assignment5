29fff456051183a7c088d0c4cc102064842cf039
Hasibmihad <mdhasibuddin12@gmail.com>
2023-09-04 17:53:57 +0600

    done in google colab for simplification

---
+++ assignment5.py
@@ -0,0 +1,106 @@
+class Request:
+    def __init__(self, timestamp, url, processing_time):
+        self.timestamp = timestamp
+        self.url = url
+        self.processing_time = processing_time
+
+    def get_timestamp(self):
+        return self.timestamp
+
+    def get_processing_time(self):
+        return self.processing_time
+
+class Server:
+    def __init__(self):
+        self.current_request = None
+        self.time_remaining = 0
+
+    def process_request(self, request):
+        self.current_request = request
+        self.time_remaining = request.get_processing_time()
+
+    def tick(self):
+        if self.current_request:
+            self.time_remaining -= 1
+            if self.time_remaining <= 0:
+                self.current_request = None
+
+import random
+
+def random_load_balancing(servers):
+    return random.choice(servers)
+
+def simulateOneServer(file):
+    with open(file, 'r') as f:
+        lines = f.readlines()
+
+    server = Server()
+    waiting_times = []
+
+    for line in lines:
+        parts = line.strip().split(', ')
+        timestamp, url, processing_time = int(parts[0]), parts[1], int(parts[2])
+        request = Request(timestamp, url, processing_time)
+
+        while timestamp > server.time_remaining:
+            server.tick()
+
+        if not server.current_request:
+            server.process_request(request)
+        else:
+            waiting_times.append(timestamp)
+
+    while server.current_request:
+        server.tick()
+
+    average_wait_time = sum(waiting_times) / len(waiting_times) if waiting_times else 0
+    return average_wait_time
+
+def simulateManyServersRandom(file, servers):
+    with open(file, 'r') as f:
+        lines = f.readlines()
+
+    server_pool = [Server() for _ in range(servers)]
+    waiting_times = [[] for _ in range(servers)]
+
+    for line in lines:
+        parts = line.strip().split(', ')
+        timestamp, url, processing_time = int(parts[0]), parts[1], int(parts[2])
+        request = Request(timestamp, url, processing_time)
+
+        # Choose a server using random load balancing
+        server = random_load_balancing(server_pool)
+
+        while timestamp > server.time_remaining:
+            server.tick()
+            server_pool.append(server)
+            server = random_load_balancing(server_pool)
+
+        if not server.current_request:
+            server.process_request(request)
+        else:
+            waiting_times[server_pool.index(server)].append(timestamp)
+
+    for server in server_pool:
+        while server.current_request:
+            server.tick()
+            server_pool.append(server)
+            server = random_load_balancing(server_pool)
+
+    total_waiting_time = sum(sum(times) for times in waiting_times)
+    total_requests = sum(len(times) for times in waiting_times)
+    average_wait_time = total_waiting_time / total_requests if total_requests else 0
+    return average_wait_time
+
+def main(file, servers=None):
+    if servers is None:
+        average_wait_time = simulateOneServer(file)
+    else:
+        average_wait_time = simulateManyServersRandom(file, servers)
+
+    print(f"Average Wait Time: {average_wait_time:.2f} seconds")
+
+if __name__ == "__main__":
+    input_file = "requests.csv"  # Replace with the actual input file path
+    num_servers = 2  # Replace with the desired number of servers
+    main(input_file, num_servers)
